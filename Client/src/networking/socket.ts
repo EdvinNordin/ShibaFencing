b78180c9a60b197a54449cc6285d2bd397b07bae
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-21 19:22:00 +0200

    Update socket.ts

--- Client/src/networking/socket.ts
+++ Client/src/networking/socket.ts
@@ -2,5 +2,5 @@ import { io, Socket } from "socket.io-client";
 
 // export const client: Socket = io("localhost:3000");
 export const client: Socket = io(
-  "https://interactivearchitecturebackend.onrender.com"
+  "https://webfightingbackend.onrender.com"
 );
