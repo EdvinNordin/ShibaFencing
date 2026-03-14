227a2e95c89916c2bde8252b8fe03b3b76f5b4b1
Edvin Nordin <nordin.edvin@gmail.com>
2026-03-14 14:21:55 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Start nginx as a background daemon to serve the static client files
-nginx
-# Start the .NET WebSocket server as the main process (PID 1 via exec).
-# This ensures Docker signals (SIGTERM/SIGKILL) are forwarded correctly.
-exec dotnet WebFighting.dll
